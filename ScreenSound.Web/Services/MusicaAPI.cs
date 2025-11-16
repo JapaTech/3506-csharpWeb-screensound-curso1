2dc20d4e0484d6dd121a1b18b2854024e25b9c30
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2025-11-16 15:59:06 -0300

    Adicionando componentes

---
+++ ScreenSound.Web/Services/MusicaAPI.cs
@@ -0,0 +1,11 @@
+﻿namespace ScreenSound.Web.Services
+{
+    public class MusicaAPI
+    {
+        private readonly HttpClient _httpClient;
+        public MusicaAPI(IHttpClientFactory factory)
+        {
+            _httpClient = factory.CreateClient("API");
+        }
+    }
+}
