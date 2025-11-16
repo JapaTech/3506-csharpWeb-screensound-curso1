77065689be05419ae89796d40e8a5d3fa5660f13
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2025-11-16 19:24:36 -0300

    Adcionando e criando o tema da blazor

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -2,12 +2,17 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScreenSound.Web;
 using ScreenSound.Web.Services;
+using MudBlazor;
+using MudBlazor.Services;
+using MudBlazor.Utilities;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
 builder.Services.AddTransient<ArtistaAPI>();
 builder.Services.AddHttpClient("API", client =>
 {
